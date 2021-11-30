7f03ad753e1859e18a9232f85e58f7933e367dfd
Lindsey Pregent <lindsey.pregent@gmail.com>
2021-11-30 16:58:29 -0500

    completed assignment

---
+++ index.js
@@ -0,0 +1,51 @@
+function add(number1, number2) {
+    return number1+number2;
+}
+
+function subtract(number1, number2) {
+    return number1-number2;
+}
+
+function multiply(number1, number2) {
+    return number1*number2;
+}
+function divide(number1, number2) {
+    return number1/number2;
+}
+function increment(n) {
+    return n+=1;
+}
+
+function decrement(n) {
+    return n-=1;
+}
+
+function makeInt(n) {
+    parseInt (n, 10);
+    return n;
+}
+
+function presserveDecimal(n) {
+    parseFloat (n, 10);
+    return n;
+}
+
+function makeInt(n) {
+    parseInt (n, 10);
+    return n;
+}
+
+function preserveDecimal(n) {
+    parseFloat(n, 10)
+    return n;
+}
+
+function makeInt(string) {
+    parseFloat(n, 10)
+    return n;
+}
+
+function preserveDecimal(string) {
+    parseFloat(n, 10)
+    return n;
+}
